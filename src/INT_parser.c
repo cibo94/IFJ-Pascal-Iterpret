286f072d7de75b996071fb53ac2b6f38da0730bd
Onionka <onionka@a05-0902a.kn.vutbr.cz>
2014-11-24 18:59:54 +0100

    Lukasov union pridany

--- src/INT_parser.c
+++ src/INT_parser.c
@@ -3,44 +3,49 @@
 typedef enum {
     TERM_INT,
     TERM_REAL,
-    TERM_STRING
+    TERM_STRING,
+    TERM_LABEL
 } ETermType;
 
 typedef struct {
-    int    ival;
-    float  dval;
-    char  *sval;
+    union {
+        int       integer;
+        float     real;
+        char *    string;
+        uint32_t  address;
+    } value;
     ETermType type;
 } TTerm;
 
 static void plus (TTerm *op1, TTerm *op2, TTerm *ret) {
     switch (ret->type) {
         case TERM_INT    :
-            ret->ival = op1->ival + op2->ival;
+            ret->value.integer = op1->value.integer + op2->value.integer;
         break;
             
         case TERM_REAL   :
-            ret->dval = op1->dval + op2->dval;
+            ret->value.real = op1->value.real + op2->value.real;
         break;
 
         case TERM_STRING :
-            ret->sval = strcat(op1->sval, op2->sval);
+            ret->value.string = strcat(op1->value.string, op2->value.string);
         break;
+
+        default          : break;
     }
 }
 
 static void minus (TTerm *op1, TTerm *op2, TTerm *ret) {
      switch (ret->type) {
         case TERM_INT    :
-            ret->ival = op1->ival - op2->ival;
+            ret->value.integer = op1->value.integer - op2->value.integer;
         break;
             
         case TERM_REAL   :
-            ret->dval = op1->dval - op2->dval;
+            ret->value.real = op1->value.real - op2->value.real;
         break;
 
-        case TERM_STRING :
-        break;
+        default          :  break;
     }
 }
 
